2361f1c06429fa1ebb44757cf4367f5a48f06af1
talk-shit <talkshit@elsasd.com>
2024-09-03 20:06:57 -0700

    Update calorie-analysis.js

--- api/calorie-analysis.js
+++ api/calorie-analysis.js
@@ -1,1 +1,11 @@
 // JavaScript for API 
+export default function handler(req, res) {
+    console.log("Function received a request.");
+    if (req.method === 'POST') {
+        console.log("POST request received with body:", req.body);
+        res.status(200).json({ message: "Success", data: req.body });
+    } else {
+        console.log("Received non-POST request.");
+        res.status(405).json({ message: "Method Not Allowed" });
+    }
+}
